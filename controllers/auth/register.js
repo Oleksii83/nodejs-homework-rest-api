5f44304cf02eb49c7370bc242c4357e1fccae43a
Oleksii83 <zaaytav@gmail.com>
2021-12-05 11:21:01 +0200

    langEngl

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,37 +1,37 @@
-const { Conflict } = require('http-errors')
-const gravatar = require('gravatar')
-const { nanoid } = require('nanoid')
-const { sendMail } = require('../../helpers')
+const { Conflict } = require("http-errors");
+const gravatar = require("gravatar");
+const { nanoid } = require("nanoid");
+const { sendMail } = require("../../helpers");
 
-const { User } = require('../../model')
+const { User } = require("../../model");
 
 const register = async (req, res) => {
-  const { email, password } = req.body
-  const user = await User.findOne({ email })
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
 
   if (user) {
-    throw new Conflict(`User with email=${email} allready exist`)
+    throw new Conflict(`User with email=${email} allready exist`);
   }
-  const verificationToken = nanoid()
-  const avatar = gravatar.url(email)
-  const newUser = new User({ email, avatar, verificationToken })
+  const verificationToken = nanoid();
+  const avatar = gravatar.url(email);
+  const newUser = new User({ email, avatar, verificationToken });
 
-  newUser.setPassword(password)
-  await newUser.save()
+  newUser.setPassword(password);
+  await newUser.save();
 
   const mail = {
     to: email,
-    subject: 'Подтверждение регистрации',
-    html: `<a href="http://localhost:3000/api/auth/verify/${verificationToken}">Нажмите для подтверждения</a>`,
-  }
+    subject: "Registration confirmation",
+    html: `<a href="http://localhost:3000/api/auth/verify/${verificationToken}">Click here to confirm your email</a>`,
+  };
 
-  await sendMail(mail)
+  await sendMail(mail);
 
   res.status(201).json({
-    status: 'success',
+    status: "success",
     code: 201,
-    message: 'Register success',
-  })
-}
+    message: "Register success",
+  });
+};
 
-module.exports = register
+module.exports = register;
